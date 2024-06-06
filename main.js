c9ca0a0b701526453a47cc09a52371691e2c84e5
Igor Santana <santanna.ig@hotmail.com>
2024-06-06 10:11:13 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        autoplay:true,
+        loop:true,
+        autoplayTimeout: 10000,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        autoplaySpeed: 800,
+        slideSpeed: 500,
+        mouseDrag: false
+    });
+  });
